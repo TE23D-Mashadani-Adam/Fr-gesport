6d6d38ec54894cbd4f125769ff61206be44e151c
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2024-09-06 10:14:54 +0200

    Skriver frågorna och readline

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Väkommen till frågesport, du kommer att få svara på tre frågor, försök svara rätt på så "+
+"många som du kan, lycka till!");
+
+int poäng = 0;
+
+//Frågor
+Console.WriteLine("Vilken är den bästa fotbollspelaren i historien? a) Ronaldo b) Messi c) Bellingham");
+String ans1 = Console.ReadLine();
+Console.WriteLine("Vem vann VM 2022? a)Franktike b) Argentina c) Spanien");
+String ans2 = Console.ReadLine();
+Console.WriteLine("Vad är antalet spelare i varje lag i en fotbollsmatch? a) 10 b) 11 c) 12");
+String ans3 = Console.ReadLine();
+
+
+
