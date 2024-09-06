cc68335a1a553ea8aee79196f45c29ec3a620393
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2024-09-06 11:06:04 +0200

    ajdbksdlasjn

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -5,21 +5,47 @@ Console.WriteLine("Väkommen till frågesport, du kommer att få svara på tre f
 
 int poäng = 0;
 
+String ans1 = "";
+String ans2 = "";
+String ans3 = "";
+
+bool wrongFormat;
+
+
 //Frågor
+
+while(true){
 Console.WriteLine("Vilken är den bästa fotbollspelaren i historien? a) Ronaldo b) Messi c) Bellingham");
-String ans1 = Console.ReadLine();
-if(ans1=="a"){
+ans1 = Console.ReadLine();
+if(ans1 == "b" || ans1 == "a" || ans1 == "c"){
+    if(ans1=="a"){
     poäng += 1;
+    }
+    break;
 }
+}
+
+
+while(true){
 Console.WriteLine("Vem vann VM 2022? a)Franktike b) Argentina c) Spanien");
-String ans2 = Console.ReadLine();
-if(ans2=="b"){
+ans2 = Console.ReadLine();
+if(ans2 == "b" || ans2 == "a" || ans2 == "c"){
+    if(ans2=="b"){
     poäng += 1;
 }
+    break;
+}
+}
+
+while(true){
 Console.WriteLine("Vad är antalet spelare i varje lag i en fotbollsmatch? a) 10 b) 11 c) 12");
-String ans3 = Console.ReadLine();
-if(ans3=="b"){
+ans3 = Console.ReadLine();
+if(ans3 == "b" || ans3 == "a" || ans3 == "c"){
+    if(ans3=="b"){
     poäng += 1;
+    break;
+}
+}
 }
 
 Console.WriteLine("\n");
