f415017353b99a0384faebeed330b008c67882d8
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2024-09-06 10:24:19 +0200

    En poäng för rätt svar på varje fråga

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Väkommen till frågesport, du kommer att få svara på tre frågor, försök svara rätt på så "+
+﻿using System.Diagnostics;
+
+Console.WriteLine("Väkommen till frågesport, du kommer att få svara på tre frågor, försök svara rätt på så "+
 "många som du kan, lycka till!");
 
 int poäng = 0;
@@ -6,10 +8,21 @@ int poäng = 0;
 //Frågor
 Console.WriteLine("Vilken är den bästa fotbollspelaren i historien? a) Ronaldo b) Messi c) Bellingham");
 String ans1 = Console.ReadLine();
+if(ans1=="a"){
+    poäng += 1;
+}
 Console.WriteLine("Vem vann VM 2022? a)Franktike b) Argentina c) Spanien");
 String ans2 = Console.ReadLine();
+if(ans2=="b"){
+    poäng += 1;
+}
 Console.WriteLine("Vad är antalet spelare i varje lag i en fotbollsmatch? a) 10 b) 11 c) 12");
 String ans3 = Console.ReadLine();
+if(ans3=="b"){
+    poäng += 1;
+}
+
+
 
 
 
